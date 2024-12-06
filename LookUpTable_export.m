31fc3182db0c46858d3fd7f3a923d6850ea22f7d
JoestHomann <st171800@stud.uni-stuttgart.de>
2024-12-06 16:07:47 +0100

    Rudimentary matrices export

--- LookUpTable_export.m
+++ LookUpTable_export.m
@@ -2,6 +2,7 @@ clear;
 
 tic;
 
+
 modName = ['sat_test'];
 % Path to model file
 ADBSat_path = ADBSat_dynpath;
@@ -11,11 +12,8 @@ resOut = fullfile(ADBSat_path,'inou','results');
 
 %Input conditions
 alt = 200; %km
-inc = 51.6; %deg
+inc = 0; %deg
 env = [alt*1e3, inc/2, 0, 106, 0, 65, 65, ones(1,7)*3, 0]; % Environment variables
-
-aoa = -5:1:5; % Angle of attack
-aos = -5:1:5; % Angle of sideslip
 
 % Model parameters
 shadow = 1;
@@ -30,11 +28,36 @@ inparam.sol_cD = 0.25; % Diffuse Reflectivity
 verb = 0; % Verbose
 del = 1; % Delete temp
 
+% Start Parameters
+n=90;
+aoa = 1:1:n; % Angle of attack
+aos = 1:1:n; % Angle of sideslip
+
 % Import model
 modOut = ADBSatImport(modIn, modOut, verb);
 
 % Coefficient Calculation
 output = ADBSatFcn( modOut, resOut, inparam, aoa, aos, shadow, solar, env, del, verb );
+
+% Table-initialisation
+LookUpTable=zeros(n,n);
+
+load(output,'aedb');
+
+while aoa <= n
+    while aos <= n
+        
+        LookUpTable(aoa,aos)=aedb.aero.Cf_wX;
+        disp(aos)
+        aos=aos+1;
+
+    end
+    aos=1;
+    aoa=aoa+1;
+
+end
+
+disp(LookUpTable)
 
 % Plot surface distribution C_D
 figure
@@ -44,17 +67,5 @@ colorbar
 xlabel('Angle of Sideslip')
 ylabel('Angle of Attack')
 title('Drag Force Coefficient')
-
-
-% Plot surface distribution C_L
-figure
-load(output,'aedb')
-contourf((aedb.aos).*(180/pi), aedb.aoa.*(180/pi), aedb.aero.Cf_fX)
-colorbar
-xlabel('Angle of Sideslip')
-ylabel('Angle of Attack')
-title('Lift Force Coefficient')
-
-disp(aedb.aero.Cf_wX)
 
 toc;
