240c6899b2328a9eaa38acb3fb1e80adc1a760b4
Joest Homann <st171800@stud.uni-stuttgart.de>
2024-12-09 17:46:36 +0100

    Upload c_d_func

---
+++ C_D_func.m
@@ -0,0 +1,48 @@
+%Drag calc function calling test
+
+tic;
+
+modName = ['source_simplified'];
+% Path to model file
+ADBSat_path = ADBSat_dynpath;
+modIn = fullfile(ADBSat_path,'inou','obj_files',[modName,'.obj']);
+modOut = fullfile(ADBSat_path,'inou','models');
+resOut = fullfile(ADBSat_path,'inou','results');
+
+%Input conditions
+alt = 300; %km
+inc = 0; %deg
+env = [alt*1e3, inc/2, 0, 106, 0, 165, 165, ones(1,7)*15, 0]; % Environment variables
+
+% Model parameters
+shadow = 1;
+inparam.gsi_model = 'sentman';
+inparam.alpha = 1; % Accommodation (altitude dependent)
+inparam.Tw = 695; % Wall Temperature [K]
+
+solar = 1;
+inparam.sol_cR = 0.15; % Specular Reflectivity
+inparam.sol_cD = 0.25; % Diffuse Reflectivity
+
+verb = 0; % Verbose
+del = 1; % Delete temp
+
+% Start Parameters
+aoa = -45:1:45; % Angle of attack vector
+aos = -45:1:45; % Angle of sideslip vector
+n=length(aoa);
+disp(n)
+
+% Import model
+modOut = ADBSatImport(modIn, modOut, verb);
+
+output = ADBSatFcn( modOut, resOut, inparam, aoa, aos, shadow, solar, env, del, verb );
+
+load(output,"aedb");
+c_d_matrix=[[0; aoa'], [aos; aedb.aero.Cf_wX]];
+
+disp(c_d_matrix)
+disp(c_d_matrix(1,2))
+save('c_d_values.mat', 'c_d_matrix'); % Speichert die Matrix A in der Datei "matrix_data.mat
+
+toc;
--- lookUpTable/LookUpTable_export.m
+++ lookUpTable/LookUpTable_export.m
@@ -10,15 +10,15 @@ modOut = fullfile(ADBSat_path,'inou','models');
 resOut = fullfile(ADBSat_path,'inou','results');
 
 %Input conditions
-alt = 300; %km
+alt = 200; %km
 inc = 0; %deg
-env = [alt*1e3, inc/2, 0, 106, 0, 165, 165, ones(1,7)*15, 0]; % Environment variables
+env = [alt*1e3, inc/2, 0, 106, 0, 65, 65, ones(1,7)*3, 0]; % Environment variables
 
 % Model parameters
 shadow = 1;
 inparam.gsi_model = 'sentman';
 inparam.alpha = 1; % Accommodation (altitude dependent)
-inparam.Tw = 695; % Wall Temperature [K]
+inparam.Tw = 300; % Wall Temperature [K]
 
 solar = 1;
 inparam.sol_cR = 0.15; % Specular Reflectivity
@@ -28,36 +28,31 @@ verb = 0; % Verbose
 del = 1; % Delete temp
 
 % Start Parameters
-range = 5;
-aoa = -range:1:range; % Angle of attack vector
-aos = -range:1:range; % Angle of sideslip vector
-n=length(aoa);
-disp(n)
+n=90;
+aoa = 1:1:n; % Angle of attack
+aos = 1:1:n; % Angle of sideslip
 
 % Import model
 modOut = ADBSatImport(modIn, modOut, verb);
 
 % Coefficient Calculation
-%output = ADBSatFcn( modOut, resOut, inparam, aoa, aos, shadow, solar, env, del, verb );
+output = ADBSatFcn( modOut, resOut, inparam, aoa, aos, shadow, solar, env, del, verb );
 
 % Table-initialisation
 LookUpTable=zeros(n,n);
 
+load(output,'aedb');
 
-i=1;
-j=1;
-
-while i <= n
-    while j <= n
-        output = calc_coeff( modOut, resOut, aoa, aos, inparam, shadow, solar, del, verb );
-        load(output,'aedb');
-        LookUpTable(i,j)=aedb.aero.Cf_wX;
+while aoa <= n
+    while aos <= n
+        
+        LookUpTable(aoa,aos)=aedb.aero.Cf_wX;
         disp(aos)
-        j=j+1;
+        aos=aos+1;
 
     end
-    j=1;
-    i=i+1;
+    aos=1;
+    aoa=aoa+1;
 
 end
 
